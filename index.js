4faa9896ae764cbdec8c10f83f26e89b0a0ebeec
denibudeyko <denbudeyko@gmail.com>
2021-05-14 06:52:02 +0300

    fix

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = 8000
 
 app.get('/', (req,res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running  at https://localhost:${PORT}`);
 });
